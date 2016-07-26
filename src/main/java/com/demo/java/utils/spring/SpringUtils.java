8a752d327e3e3427fd1a6132fd0801464d659db4
zhanghanlin <andyzhanghanlin@163.com>
2016-07-26 17:07:29 +0800

    Spring工具类

---
+++ src/main/java/com/demo/java/utils/spring/SpringUtils.java
@@ -0,0 +1,35 @@
+package com.demo.java.utils.spring;
+
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+
+/**
+ * Spring工具类
+ */
+public class SpringUtils implements ApplicationContextAware {
+
+    private static ApplicationContext applicationContext;
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.applicationContext = applicationContext;
+    }
+
+    public static ApplicationContext getApplicationContext() {
+        return applicationContext;
+    }
+
+    /**
+     * 获取spring配置的bean
+     *
+     * @param beanName
+     * @param <T>
+     * @return
+     */
+    public static <T> T getBean(String beanName) {
+        if (getApplicationContext() == null)
+            return null;
+        return (T) getApplicationContext().getBean(beanName);
+    }
+}
